f4504395d20844ac6d901566443b88f755ad1010
sunuo <sunuo1997@163.com>
2021-08-31 23:15:07 +0800

    爬虫test

--- paper_info.py
+++ paper_info.py
@@ -1,4 +1,6 @@
 import json
+import requests
+from bs4 import BeautifulSoup
 
 def readJson(path):
     file = open(path,"r")
@@ -6,7 +8,37 @@ def readJson(path):
     result = fileJson['result']
     hits = result["hits"]
     hit = hits['hit']
-    print(hit)
+    return hit
 
-readJson("paper_info.json")
+
+for i in readJson("paper_info.json"):
+    #todo：作者，标题已经知道
+    info = i['info']
+    authors = info['authors']['author']
+    name_list = []
+    for a in authors:
+        name_list.append(a['text'])
+    print(name_list)
+    title = info['title']
+
+    url = info['ee']
+    print(url)
+    try:
+        kv = {'user-agent': 'Mozilla/5.0'}  # 应对爬虫审查
+        r = requests.get(url, headers=kv)
+        r.raise_for_status()  # 若返回值不是202，则抛出一个异常
+        r.encoding = r.apparent_encoding
+    except:
+        print("进入网站失败")
+    demo = r.text
+
+    soup = BeautifulSoup(demo, "html.parser")
+    t = soup.find('time')
+    print(t)
+    #todo:获取关键字
+    keywords = soup.find_all("<span class=\"Keyword\">")
+    print(keywords)
+    #获取摘要
+    # abstract = soup.find(id = 'Par1')
+    print('-----------------------------')
 
